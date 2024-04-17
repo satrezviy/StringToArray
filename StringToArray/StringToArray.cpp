88b5c7d5ec76bde162d52eb1632c638d1a4a152f
Mokro-Dev <Mokro@vm>
2024-04-17 09:46:13 +0300

    Implemented Convert for string and wstring

--- StringToArray/StringToArray.cpp
+++ StringToArray/StringToArray.cpp
@@ -1,20 +1,41 @@
-// StringToArray.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
 
-int main()
-{
-    std::cout << "Hello World!\n";
+using namespace std;
+
+string sInput = "";
+wstring wInput = L"Profile";
+
+void Convert(string &s);
+void Convert(wstring w);
+
+int main() {
+	//Convert(sInput);
+	Convert(wInput);
 }
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
+void Convert(string &s) {
+	string res = "{ ";
+	for (size_t i = 0; i < s.size(); i++) {
+		string temp = "'";
+		if (s[i] == '\\') temp += s[i];
+		temp += s[i];
+		temp += "', ";
+		res += temp;
+	}
+	res[res.size() - 2] = ' ';
+	res[res.size() - 1] = '}';
+	cout << res << "\n";
+}
+void Convert(wstring w) {
+	wstring res = L"{ ";
+	for (size_t i = 0; i < w.size(); i++) {
+		wstring temp = L"L'";
+		if (w[i] == '\\') temp += w[i];
+		temp += w[i];
+		temp += L"', ";
+		res += temp;
+	}
+	res[res.size() - 2] = L' ';
+	res[res.size() - 1] = L'}';
+	wcout << res << L"\n";
+}
